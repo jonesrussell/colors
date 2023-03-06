7f34167a8fb7d206e615677f1b8a7a87b19b900a
Russell Jones <jonesrussell42@gmail.com>
2023-03-06 17:50:25 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Index from './Index.vue'
+</script>
+
+<template>
+  <main>
+    <Index/>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: slategray;
+  }
+</style>
